3e25ebdc14ea78cb4eb4ff4107aa294c28bba1f7
ads-Liushenko-kp13 <92879216+ads-Liushenko-kp13@users.noreply.github.com>
2022-02-15 18:19:59 +0200

    Add files via upload

---
+++ LAB1/WpfApp1/Calc.xaml.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Data;
+
+namespace WpfApp1
+{
+    public partial class Calc : Window
+    {
+        public Calc()
+        {
+            InitializeComponent();
+
+            foreach (UIElement el in BaseGrid.Children)
+            {
+                if (el is Button)
+                {
+                    ((Button)el).Click += Button_Click;
+                }
+            }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string str = (string)((Button)e.OriginalSource).Content;
+            if (str == "AC")
+            {
+                Summary.Text = " ";
+            }
+            else if (str == "=")
+            {
+                string result = new DataTable().Compute(Summary.Text,null).ToString();
+                Summary.Text = result;
+            }
+            else if (str == "Exit")
+            {
+                MainWindow mw;
+                Hide();
+
+                mw = new MainWindow();
+               
+                mw.Show();
+            }
+            else
+            {
+                Summary.Text += str;
+            }
+        }
+    }
+}
---
+++ LAB1/WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            TTT.Click += Button_Click_2;
+            TTT.Click -= Button_Click;
+        }
+
+        private void Button_Click (object sender, RoutedEventArgs e)
+        {
+
+            Calc mw;
+            Hide();
+            mw = new Calc();
+            
+            mw.Show();
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            TicTacToe TicTacToe;
+            TicTacToe = new TicTacToe();
+            Hide();
+            TicTacToe.Show();
+        }
+
+        private void Std_Click(object sender, RoutedEventArgs e)
+        {
+            Students mw;
+            mw = new Students();
+            Hide();
+            mw.Show();
+        }
+
+        private void AUT_Click(object sender, RoutedEventArgs e)
+        {
+            Author Author;
+            Author = new Author();
+            Hide();
+            Author.Show();
+        }
+    }
+}
